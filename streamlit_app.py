76f96e7224e94f096604f90710900efcc7e0d59e
Kiersten South <kierstensouth@Kierstens-MacBook-Pro.local>
2023-12-06 15:12:30 -0500

    all notebooks checked. final push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 #Imports
 import pickle
-import streamlit_app as st
+import streamlit as st
 
 st.title('Will This Book Save You From Your Book Hangover?!')
+
+st.write('testing')
+
+#Load pickled pipeline so we can take inouts and make predictions
+with open('../code/book_pipe.pkl', 'rb') as pickle_in:
+    pipe = pickle.load(pickle_in)
+
+# get user input
+user_text = st.text_input('Please input some text: ')
+
+#now generate predictions
+predicted = pipe.predict([user_text])[0]
+
+#display predictions
+st.write(f'Your prediction is: {predicted}')
